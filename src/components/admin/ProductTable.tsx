21e705ba04c406d05b6c1218e804e57903edb779
Ferminju <ferminjuliancruz26@gmail.com>
2025-10-23 12:15:02 -0300

    stock admin eliminado

--- src/components/admin/ProductTable.tsx
+++ src/components/admin/ProductTable.tsx
@@ -69,9 +69,6 @@ export default function ProductTable({
                                 Precio
                             </th>
                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Stock
-                            </th>
-                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Destacado
                             </th>
                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
